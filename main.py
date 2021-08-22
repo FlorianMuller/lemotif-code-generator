32180fdd7dd121b204dd685c13b4313abf8d388a
Florian Muller <fmuller@student.42.fr>
2021-08-22 22:59:34 +0200

    saving result in files with multiple formating

--- main.py
+++ main.py
@@ -1,25 +1,60 @@
 import json
 
 data = [
-	["d"],
-	["a"],
-	["a"],
-	["w", "W"],
-	["S", "s"],
-	["R"],
-	["R"],
-	["u"],
-	["w", "W"],
-	["0", "EI", "IE", "E", "e", "O"],
-	["u"],
-	# ["AU", "UA", "au", "ua", "A", "U", "a", "u"],
-    # ["p"],
-    # ["R"],
-    # ["A", "C"],
-    # ["R"],
-    # ["S"],
-    # ["n"]
+    # 1
+    ["d"],
+    # 2
+    ["a"],
+    # 3
+    ["a"],
+    # 4
+    ["w", "W"],
+    # 5
+    ["S", "s"],
+    # 6
+    ["R"],
+    # 7
+    ["R"],
+    # 8
+    ["u"],
+    # 9
+    ["w", "W"],
+    # 10
+    ["0", "EI", "IE", "E", "e", "O"],
+    # 11
+    ["u"],
+    # 12
+    ["AU", "UA", "au", "ua", "A", "U", "a", "u"],
+    # 13
+    ["p"],
+    # 14
+    ["R"],
+    # 15
+    ["A", "C"],
+    # 16
+    ["R"],
+    # 17
+    ["S"],
+    # 18
+    ["n"]
 ]
+
+def format_for_youtube(base_lst):
+    new_lst = ['https://www.youtube.com/watch?v=' + res[:11] for res in base_lst]
+    # removing duplicate
+    new_lst = list(set(new_lst))
+
+    return new_lst
+
+def format_for_unsonparjour(base_lst):
+    return ['https://www.unsonparjour.com/' + res for res in base_lst]
+
+def save_files(name, result_lst):
+    with open(f"{name}.json", "w") as f:
+        json.dump({"data": data, "results": result_lst}, f)
+    
+    with open(f"{name}.txt", "w") as f:
+        f.write("Résultats:\n" + '\n'.join(result_lst) + "\n\n\nParamètres:\n" + json.dumps(data, indent=4))
 
 def next_index(index_lst):
     for i, index in list(enumerate(index_lst))[::-1]:
@@ -32,7 +67,7 @@ def next_index(index_lst):
     
     return index_lst
 
-def main(youtube_format, print_res, save):
+def main(print_res, save):
     index_lst = [0] * len(data)
     res_lst = []
 
@@ -41,24 +76,15 @@ def main(youtube_format, print_res, save):
         next_index(index_lst)
         res_lst.append(''.join([possibility[index_lst[i]] for i, possibility in enumerate(data)]))
 
-    # Formating results
-
-    if youtube_format:
-        res_lst = ['https://www.youtube.com/watch?v=' + res[:11] for res in res_lst]
-        # removing duplicate
-        res_lst = list(set(res_lst))
-    
     if print_res:
         print('\n'.join(res_lst))
     
     if save:
-        with open("result.json", "w") as f:
-            json.dump({"data": data, "results": res_lst}, f)
-        
-        with open("result.txt", "w") as f:
-            f.write("Résultats:\n" + '\n'.join(res_lst) + "\n\n\nParamètres:\n" + json.dumps(data, indent=4))
+        save_files("result", res_lst)
+        save_files("youtube_result", format_for_youtube(res_lst))
+        save_files("unsonparjour_result", format_for_unsonparjour(res_lst))
 
     print(f'\n\n {len(res_lst)} result found')
 
 if __name__ == '__main__':
-    main(True, True, True)
+    main(True, True)
